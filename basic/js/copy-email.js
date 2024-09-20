5748b653650faafcb6f57a93aaa457472ba9bf6f
APIvahnenko <apivahnenko@gmail.com>
2024-09-20 12:52:11 +0100

    copy link fix, working site

--- basic/js/copy-email.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".copy-link").forEach((copyLinkParent) => {
-    copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText("anna.ivakh@gmail.com");
-
-        document.getElementById("copy-confirm").style.display="block";
-        setTimeout(() => (document.getElementById("copy-confirm").style.display="none"), 2000);
-    });
-});
--- basic/js/script.js
+++ basic/js/script.js
@@ -12,3 +12,10 @@ function onload() {
 function copyURL() {
     navigator.clipboard.writeText(window.location.href)
 }
+
+function copyEmail() {
+    navigator.clipboard.writeText("anna.ivakh@gmail.com");
+
+    document.getElementById("copy-confirm").style.display="block";
+        setTimeout(() => (document.getElementById("copy-confirm").style.display="none"), 2000);
+}
